d8c4770b0a3c2cc2c4773f2e0a1fb1d70f8179bd
yogiprayogakadek <yogiprayogakadek@gmail.com>
2022-07-18 23:06:15 +0800

    Chart Update - Controller

--- app/Http/Controllers/Main/VotingController.php
+++ app/Http/Controllers/Main/VotingController.php
@@ -13,18 +13,24 @@ class VotingController extends Controller
     {
         $hasVoted = false;
         $checkIfVoted = Pemilu::where('status', true)->first();
-        if($checkIfVoted->data_pemilu != null) {
-            foreach(json_decode($checkIfVoted->data_pemilu)->data as $key => $value) {
-                if(auth()->user()->id_user == $value->id_user) {
-                    $hasVoted = true;
+        $ifNotNull = Pemilu::where('status', true)->first();
+        if($ifNotNull) {
+            $showChart = json_decode($checkIfVoted->data_pemilu)->result ?? false;
+            if($checkIfVoted->data_pemilu != null) {
+                foreach(json_decode($checkIfVoted->data_pemilu)->data as $key => $value) {
+                    if(auth()->user()->id_user == $value->id_user) {
+                        $hasVoted = true;
+                    }
                 }
             }
+        } else {
+            $hasVoted = false;
+            $showChart = false;
         }
-
+    
         // data kandidat untuk chart
         $chart = $this->chart();
         // dd($chart);
-        $showChart = json_decode($checkIfVoted->data_pemilu)->result ?? false;
 
         $pemilu = Pemilu::where('status', true)->pluck('id_pemilu')->toArray();
         if(count($pemilu) > 0){
@@ -70,7 +76,12 @@ class VotingController extends Controller
 
     public function chart()
     {
-        $pemilu = Pemilu::where('status', true)->first();
+        $ifNotNull = Pemilu::where('status', true)->first();
+        if($ifNotNull) {
+            $pemilu = Pemilu::where('status', true)->first();
+        } else {
+            $pemilu = Pemilu::latest()->first();
+        }
         $kandidat = Kandidat::where('id_pemilu', $pemilu->id_pemilu)->get();
         
         if($pemilu->data_pemilu != null) {
